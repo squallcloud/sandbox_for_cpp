da60536cd1106192152ec75fcef2e1054b47d8d6
squallcloud <squallcloudzidane@gmail.com>
2024-09-29 13:24:25 +0900

    [update] 定義済みマクロ用

--- feature_sample/main.cpp
+++ feature_sample/main.cpp
@@ -10,11 +10,17 @@ import ex_thread;
 import ex_field_init;
 import ex_multi_array;
 import ex_template_meta_program;
+import ex_defined_macro;
 
 int main(void)
 {
+    struct FuncInfo {
+        std::string str{};
+        std::function<void()> func{};
+    };
+
 #define __DEF_INFO(name) #name, &name::Exec,
-    std::unordered_map<std::string, std::function<void()>> s_func_list {
+    std::vector<FuncInfo> s_func_list{
         { __DEF_INFO(ex_foreach) },
         { __DEF_INFO(ex_tuple) },
         { __DEF_INFO(ex_initializer_lists) },
@@ -22,12 +28,13 @@ int main(void)
         { __DEF_INFO(ex_field_init) },
         { __DEF_INFO(ex_multi_array) },
         { __DEF_INFO(ex_template_meta_program) },
+        { __DEF_INFO(ex_defined_macro) },
     };
 #undef __DEF_INFO
 
     for (auto& func_info : s_func_list) {
-        const auto& mod_name = func_info.first;
-        const auto& func = func_info.second;
+        const auto& mod_name = func_info.str;
+        const auto& func = func_info.func;
 
         ex::Dummy::ResetCounter();
         ::printf("[%s] \n", mod_name.c_str());
