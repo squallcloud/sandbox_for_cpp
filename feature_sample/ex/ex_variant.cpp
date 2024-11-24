bc733bf689a1b629c6a6292123b2e4b3bfe3fa23
squallcloud <squallcloudzidane@gmail.com>
2024-11-24 13:55:18 +0900

    [update] variant

---
+++ feature_sample/ex/ex_variant.cpp
@@ -0,0 +1,26 @@
+﻿import ex;
+
+import dummy;
+#include <variant>
+
+namespace ex_variant {
+
+void Exec()
+{
+    std::variant<ex::Dummy, int, short> var;
+    var = 10;
+
+    ::printf("index = %llu \n", var.index());
+
+    var = ex::Dummy();
+
+    ::printf("index = %llu \n", var.index());
+
+    var = (short)10;
+
+    ::printf("index = %llu \n", var.index());
+
+    ::printf("var size = %llu \n", sizeof(var));
+}
+
+}//ex_variant
